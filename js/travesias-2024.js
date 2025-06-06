bbcd57fc88ca0f7737861d9af43d211f4c118b64
cdalandalus <168721666+cdalandalus@users.noreply.github.com>
2025-06-06 21:06:11 +0200

    Add files via upload

---
+++ js/travesias-2024.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Selecciona el elemento donde quieres insertar el fragmento de HTML
+    var contenedor = document.getElementById("travesias-2024");
+
+    // Carga el contenido del archivo fragmento.html
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // Inserta el contenido dentro del elemento contenedor
+            contenedor.innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "../include/travesias-2024.html", true);
+    xhttp.send();
+});
